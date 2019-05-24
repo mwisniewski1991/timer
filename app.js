72319d0f32593e01c9de688155cc72d0297562b5
mwisniewski1991 <m.wisniewski1991@gmail.com>
2019-05-25 00:24:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 timeController = (function(){
 
     const timeData = {
@@ -31,7 +30,13 @@ timeController = (function(){
             }
         },
 
-        test: function(){
+        updateTimeCount: function(mins, secs, msec){
+            timeData.mins = mins;
+            timeData.secs = secs;
+            timeData.msec = msec;
+        },
+
+        getData: function(){
             return timeData;
         }
     }
@@ -45,9 +50,8 @@ UIcontroller = (function(){
         mins: '.time-box__num--mins',
         secs: '.time-box__num--secs',
         msec: '.time-box__num--msec',
-        buttonsChange: '.btn--change' 
-
-
+        buttonsChange: '.btn--change',
+        startButton: '#start'
     }
 
     return{
@@ -57,7 +61,11 @@ UIcontroller = (function(){
 
             //destructuring array
             // [mins, secs, msec] = arr;
-            {mins, secs, msec = obj};
+            let {mins, secs, msec} = obj;
+
+            mins < 10 ? mins = `0${mins}` : mins = `${mins}`; 
+            secs < 10 ? secs = `0${secs}` : secs = `${secs}`;
+            msec < 10 ? msec = `0${msec}`.substring(0,2) : msec = `${msec}`.substring(0,2);
 
             //SELECT AND UPDATE DOM
             document.querySelector(DOMstrings.mins).textContent = mins;
@@ -76,9 +84,10 @@ UIcontroller = (function(){
 
 controller = (function(timeCtrl, UICtrl){
 
+    //GET DOM FROM UI CTRL
     const DOM = UICtrl.getDOM();
 
-
+    //UPDATE DATA TIME AND UI TIME 
     const update = function(event){
         //GET TARGET ID AND SPLIT TO GET INFO WHAT CALCULATION WITH DATA IS NEEDES
         const calc = event.target.id.split('-')[0];
@@ -86,22 +95,82 @@ controller = (function(timeCtrl, UICtrl){
 
         //FUNCTION TO UPDATE DATA
         timeCtrl.updateTime(calc, type);
-        console.log(timeCtrl.test());
+
+        // console.log(timeCtrl.getData()); TEST
 
         //FUNCTION TO UDPATE DOM
-
-
+        const data = timeCtrl.getData();
+        UICtrl.updateDOM(data);
     }
 
+    //COUNTDOWN
+    const countdown = function(){
+
+        
+        //GET DATA FROM OBJECT
+        const {mins, secs, msec} = timeCtrl.getData();
+
+        // SET UP END TIME
+        const endTime = moment()
+                .add(mins, 'minutes')
+                .add(secs, 'seconds')
+                .add(msec);
+
+                const check = function(){
+
+                    //SET UP START TIME - CHANGE ON EVERY LOOP
+                    let startTime = moment();
+                    
+                    //check if end time is still higher than end
+                    if(endTime>startTime){
+            
+                        //wait moment until next counting
+                        setTimeout(loop = function(endtime){
+            
+                            //CALCULATE DIFFRENCES
+                            let mins = endTime.diff(startTime, 'minutes');
+                            let secs = endTime.diff(startTime, 'seconds') - mins * 60;  //CALCULATED HOW MUCH SECONDS LEFT
+                            let msec = endTime.diff(startTime) - (mins * 60 * 1000) - (secs * 1000); //CALCULATED HOW MUCH MILISECONDS LEFT
+                    
+                            //UPDATE DATA TIME
+                            timeCtrl.updateTimeCount(mins, secs, msec);
+                            
+                            //UPDATE DOM
+                            const data = timeCtrl.getData();
+                            UICtrl.updateDOM(data);
+            
+            
+                            //REPEAT LOOP
+                            check();
+                        }, 100);
+                    }
+                    else{
+                        //FINISH FUNCTION
+                        //UPDATE DATA TIME
+                        timeCtrl.updateTimeCount(0, 0, 0);
+                        //UPDATE DOM
+                        const data = timeCtrl.getData();
+                        UICtrl.updateDOM(data);
+                    }
+                };
+        //START CHECKING FUNCTION
+        check();
+        
+    }
+
+    //ADD EVENT LISTENERS TO BUTTONS
     const addEvents = function(){
 
+        //BUTTON -> CHANGE TIME
         const buttons = document.querySelectorAll(DOM.buttonsChange);
 
-        //ADD UPDATE BUTTON
         for(let i = 0; i < buttons.length; i++){
             buttons[i].addEventListener('click', update);
         }
 
+        //START COUNTDOWN
+        const startButton = document.querySelector(DOM.startButton);
+        startButton.addEventListener('click', countdown);
     }
 
 
@@ -114,80 +183,3 @@ controller = (function(timeCtrl, UICtrl){
 
 
 controller.init();
-
-// const test = function(){
-
-//     let startTime = moment();
-//     let endTime = moment()
-//                 .add(5, 'minutes')
-//                 .add(5, 'seconds')
-    
-//     let mins = endTime.diff(startTime, 'minutes');
-//     let secs = endTime.diff(startTime, 'seconds') - mins * 60;  //CALCULATED HOW MUCH SECONDS LEFT
-//     let msec = endTime.diff(startTime) - (mins * 60 * 1000) - (secs * 1000); //CALCULATED HOW MUCH MILISECONDS LEFT
-
-    
-//     mins < 10 ? mins = `0${mins}` : mins = `${mins}`;
-//     secs < 10 ? secs = `0${secs}` : secs = `${secs}`;
-//     msec < 10 ? msec = `0${msec}` : msec = `${msec}`;
-
-//     let result = `${mins}:${secs}:${msec}`
-
-//     // console.log(startTime);
-//     // console.log(endTime);
-//     console.log(result);
-
-// }();
-
-
-const test = function(){
-
-    //SET UP WHEN ENDS
-    const endTime = moment()
-                .add(0, 'minutes')
-                .add(5, 'seconds')
-
-    const check = function(){
-
-        //SET UP START TIME - CHANGE ON EVERY LOOP
-        let startTime = moment();
-
-        if(endTime>startTime){
-
-            setTimeout(loop = function(endtime){
-
-                //CALCULATE DIFFRENCES
-                let mins = endTime.diff(startTime, 'minutes');
-                let secs = endTime.diff(startTime, 'seconds') - mins * 60;  //CALCULATED HOW MUCH SECONDS LEFT
-                let msec = endTime.diff(startTime) - (mins * 60 * 1000) - (secs * 1000); //CALCULATED HOW MUCH MILISECONDS LEFT
-        
-                //add 0 to number less than 10 - it is necessery for UI
-                mins < 10 ? mins = `0${mins}` : mins = `${mins}`; 
-                secs < 10 ? secs = `0${secs}` : secs = `${secs}`;
-                msec < 10 ? msec = `0${msec}`.substring(0,2) : msec = `${msec}`.substring(0,2);
-
-                // let result = `${mins}:${secs}:${msec}`
-                let result = [mins, secs, msec];
-                
-                //UPDATE DOM
-                console.log(result);
-                UIcontroller.updateDOM(result) 
-
-
-                //REPEAT LOOP
-                check();
-            }, 100);
-        }
-        else{
-            let result = ['00', '00', '00'];
-            UIcontroller.updateDOM(result) 
-        }
-    }
-
-    check();
-
-};
-
-
-
-
