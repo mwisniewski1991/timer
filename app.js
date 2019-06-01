a28c21e36effbdcfb28dc35906ab7f1787335ca3
mwisniewski1991 <m.wisniewski1991@gmail.com>
2019-06-01 14:31:35 +0200

    Update script

--- app.js
+++ app.js
@@ -8,16 +8,25 @@ timeController = (function(){
 
     return {
         updateTime: function(calc, type){
+            //add time
             if(calc === "plus"){
                 timeData[type] = timeData[type] + 1;
             }
+            //minus time
             else{
-                if(timeData[type] - 1 < 0){
+                if(timeData[type] - 1 < 0){ //time can not be less than 0, so if it is zero no calculation
                     timeData[type] = 0;
                 }
                 else{
                     timeData[type] = timeData[type] - 1;
                 }
+            }
+
+            //60 secs turns into one minute
+            if(timeData.secs === 60){
+                // console.log(checktime)
+                timeData.mins = timeData.mins + 1
+                timeData.secs = 0;
             }
         },
 
@@ -80,84 +89,107 @@ controller = (function(timeCtrl, UICtrl){
     const DOM = UICtrl.getDOM();
 
     //VARIABLE TO START AND STOP COUNTDOWN
-    let countdownCheck = true;
+    let countdownCheck = false;
+
+    //REMOVE CLASS ACTIVE/DISACTIVE
+    const changeClass = function(){
+        let buttons = document.querySelectorAll(DOM.buttonsChange);
+        Array.from(buttons).forEach((cur)=>{
+            cur.classList.toggle('btn__active');
+            cur.classList.toggle('btn__disactive');
+        })
+    } 
 
     //UPDATE DATA TIME AND UI TIME 
     const update = function(event){
-        //GET TARGET ID AND SPLIT TO GET INFO WHAT CALCULATION WITH DATA IS NEEDES
-        const calc = event.target.id.split('-')[0];
-        const type = event.target.id.split('-')[1];
 
-        //FUNCTION TO UPDATE DATA
-        timeCtrl.updateTime(calc, type);
+        //CHECK STATUS OF COUNTDOWN. DO NOT ADD DURING COUNTDONW
+        if(countdownCheck == false){
 
-        // console.log(timeCtrl.getData()); TEST
-
-        //FUNCTION TO UDPATE DOM
-        const data = timeCtrl.getData();
-        UICtrl.updateDOM(data);
+            //GET TARGET ID AND SPLIT TO GET INFO WHAT CALCULATION WITH DATA IS NEEDES
+            const calc = event.target.id.split('-')[0];
+            const type = event.target.id.split('-')[1];
+            
+            //FUNCTION TO UPDATE DATA
+            timeCtrl.updateTime(calc, type);
+            
+            // console.log(timeCtrl.getData()); TEST
+            
+            //FUNCTION TO UDPATE DOM
+            const data = timeCtrl.getData();
+            UICtrl.updateDOM(data);
+        }
     }
 
     //COUNTDOWN
     const countdown = function(){
-
-        //AFTER STOPPED FUNCTION NEED TO CHANGE VAIRABLE AND TEXT CONTENT IN BUTTON
-        countdownCheck = true;
-        document.querySelector(DOM.resetButton).textContent = "Stop";
-
         
         //GET DATA FROM OBJECT
         const {mins, secs, msec} = timeCtrl.getData();
 
-        // SET UP END TIME
-        const endTime = moment()
-                .add(mins, 'minutes')
-                .add(secs, 'seconds')
-                .add(msec);
+        //CHECK IF COUNTDOWN IS ACTIVE. AVOID SITUATION WHERE START COUTNING TWICE
+        //START ONLY WHEN TIME IS BIGGER THAN 0
+        if(countdownCheck === false && mins >0 || secs>0 || msec > 0){
+            
+            //AFTER STOPPED FUNCTION NEED TO CHANGE VAIRABLE AND TEXT CONTENT IN BUTTON
+            countdownCheck = true;
+            document.querySelector(DOM.resetButton).textContent = "Stop";
 
-                const check = function(){
-
-                    if(countdownCheck){
-                        //SET UP START TIME - CHANGE ON EVERY LOOP
+            //DISACTIVE/ACTIVE BUTTONS - ONLY CSS 
+            changeClass();
+            
+            // SET UP END TIME
+            const endTime = moment()
+            .add(mins, 'minutes')
+            .add(secs, 'seconds')
+            .add(msec);
+            
+            const check = function(){
+                
+                if(countdownCheck){
+                    //SET UP START TIME - CHANGE ON EVERY LOOP
                         let startTime = moment();
                         
                         //check if end time is still higher than end
                         if(endTime>startTime){
-                
+                            
                             //wait moment until next counting
                             setTimeout(loop = function(endtime){
-                
+                                
                                 //CALCULATE DIFFRENCES
                                 let mins = endTime.diff(startTime, 'minutes');
                                 let secs = endTime.diff(startTime, 'seconds') - mins * 60;  //CALCULATED HOW MUCH SECONDS LEFT
                                 let msec = endTime.diff(startTime) - (mins * 60 * 1000) - (secs * 1000); //CALCULATED HOW MUCH MILISECONDS LEFT
-                        
+                                
                                 //UPDATE DATA TIME
                                 timeCtrl.updateTimeCount(mins, secs, msec);
                                 
                                 //UPDATE DOM
                                 const data = timeCtrl.getData();
                                 UICtrl.updateDOM(data);
-                
-                
+                                
+                                
                                 //REPEAT LOOP
                                 check();
                             }, 100);
                         }
                         else{
                             //FINISH FUNCTION
+                            countdownCheck = false
                             //UPDATE DATA TIME
                             timeCtrl.updateTimeCount(0, 0, 0);
                             //UPDATE DOM
                             const data = timeCtrl.getData();
                             UICtrl.updateDOM(data);
+                            //DISACTIVE/ACTIVE BUTTONS - ONLY CSS 
+                            changeClass();
                         }
                     }
                 };
-        //START CHECKING FUNCTION
-        check();
-        
-    }
+                //START CHECKING FUNCTION
+                check();
+            };
+    };
 
     //STOP COUNTDOWN
     const stop = function(){
@@ -168,6 +200,8 @@ controller = (function(timeCtrl, UICtrl){
             countdownCheck = false;
             //CHANGE DOM NAME FROM STOP TO RESET
             document.querySelector(DOM.resetButton).textContent = "Reset";
+            //DISACTIVE/ACTIVE BUTTONS - ONLY CSS 
+            changeClass();
         }
         else{
             //UPDATE DATA TIME
